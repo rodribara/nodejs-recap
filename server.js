f47572159e8a9932b760e2ee046c4d6dbb0008de
rodrigo <lairaravena@gmail.com>
2025-03-30 04:11:31 -0300

    intro to express

--- server.js
+++ server.js
@@ -1,106 +1,51 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 const path = require("path");
-const fs = require("fs");
-const fsPromises = fs.promises;
-
-const logEvents = require("./logEvents.js");
-const EventEmitter = require("events");
-
-class Emitter extends EventEmitter {}
-
-//initialize object
-const myEmitter = new Emitter();
-
-myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
 
 const PORT = process.env.PORT || 3500;
-const serverFile = async (filePath, contentType, response) => {
-  try {
-    const rawData = await fsPromises.readFile(
-      filePath,
-      !contentType.includes("image") ? "utf8" : ""
-    );
-    const data =
-      contentType === "application/json" ? JSON.parse(rawData) : rawData;
-    response.writeHead(filePath.includes("404.html") ? 404 : 200, {
-      "Content-Type": contentType,
-    });
-    response.end(
-      contentType === "application/json" ? JSON.stringify(data) : data
-    );
-  } catch (err) {
-    myEmitter.emit("log", `${err.name}\t${err.message}`, "errLog.txt");
-    console.log(err);
-    response.statusCode = 500;
-    response.end();
-  }
-};
-const server = http.createServer((req, res) => {
-  //runs server
-  console.log(req.url, req.method);
-  myEmitter.emit("log", `${req.method}\t${req.url}`, "reqLog.txt");
-  const extension = path.extname(req.url);
-  let contentType;
 
-  switch (extension) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".jpg":
-      contentType = "image/jpeg";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".txt":
-      contentType = "text/plain";
-      break;
-    default:
-      contentType = "text/html";
-  }
-
-  let filePath =
-    contentType === "text/html" && req.url === "/"
-      ? path.join(__dirname, "views", "index.html")
-      : contentType === "text/html" && req.url.slice(-1) === "/"
-      ? path.join(__dirname, "views", req.url, "index.html")
-      : contentType === "text/html"
-      ? path.join(__dirname, "views", req.url)
-      : path.join(__dirname, req.url);
-
-  // makes .html extension not mandatory
-  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
-
-  const fileExists = fs.existsSync(filePath);
-
-  if (fileExists) {
-    // serve file
-
-    serverFile(filePath, contentType, res);
-  } else {
-    // 404
-    switch (path.parse(filePath).base) {
-      case "old-page.html":
-        res.writeHead(301, { Location: "/new-page.html" });
-        res.end();
-        break;
-      case "www-page.html":
-        res.writeHead(301, { Location: "/" });
-        res.end();
-        break;
-      default:
-        serverFile(path.join(__dirname, "views", "404.html"), "text/html", res);
-        break;
-    }
-
-    //301 redirect
-  }
+app.get("^/$|/index(.html)?", (req, res) => {
+  // ()? means optional // ^/$ means the root path or /index.html
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+  // res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "new-page.html"); // 302 by default
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//Route handlres
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("hello html load attempt");
+    next(); // go to the next middleware function
+  },
+  (req, res) => {
+    res.send("hello html");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+const three = (req, res, next) => {
+  console.log("three");
+  res.send("Finished middleware chain"); // send response to the client
+};
+app.get(
+  "/chain(.html)?",
+  [one, two, three], // array of middleware functions
+  (req, res) => {
+    res.send("Chain middleware");
+  }
+);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
