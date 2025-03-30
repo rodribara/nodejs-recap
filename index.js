c6c8829782296df272315ac5e24c86eb57e8c41c
rodrigo <lairaravena@gmail.com>
2025-03-30 01:47:40 -0300

    added emitter

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
-console.log(format(new Date(), "yyyy-MM-dd\tHH:mm:ss"));
+const logEvents = require("./logEvents.js");
+const EventEmitter = require("events");
 
-console.log("Hello World!"); // rerendes with nodemon
+class MyEmitter extends EventEmitter {}
 
-console.log(uuid()); // if used as uuid without destructuring, you can uuid.v4() instead of uuid()
+//initialize object
+const myEmitter = new MyEmitter();
 
-console.log();
-/* 
-adding packages
-npm i nodemon -g
+// add listener for the log event
 
-npm init
+myEmitter.on("log", (msg) => logEvents(msg));
 
-deleting
- npm uninstall nodemon -g
-npm uninstall nodemon --save-dev  (or -D)
-
-*/
+setTimeout(() => {
+  //emit Event
+  myEmitter.emit("log", "log evente emitted");
+}, 2000);
---
+++ logEvents.js
@@ -0,0 +1,32 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+const fs = require("fs");
+const fsPromises = fs.promises;
+const path = require("path");
+
+const logEvents = async (message) => {
+  const dateTime = `${format(new Date(), "yyyy-MM-dd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      // check if logs directory exists
+      await fsPromises.mkdir(path.join(__dirname, "logs")); // create logs directory if it doesn't exist
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", "log.txt"),
+      logItem
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+console.log(format(new Date(), "yyyy-MM-dd\tHH:mm:ss"));
+
+console.log("Hello Worldx!");
+
+console.log(uuid());
+
+module.exports = logEvents;
